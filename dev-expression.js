fc631497a174f7e0cec908b597a27ff587ef21c7
Jimmy Jia <tesrin@gmail.com>
2019-07-06 21:29:32 -0400

    chore: Synchronize with upstream (#9)

--- dev-expression.js
+++ dev-expression.js
@@ -1,10 +1,8 @@
 /**
- * Copyright 2013-2015, Facebook, Inc.
- * All rights reserved.
+ * Copyright (c) 2013-present, Facebook, Inc.
  *
- * This source code is licensed under the BSD-style license found in the
- * LICENSE file in the root directory of this source tree. An additional grant
- * of patent rights can be found in the PATENTS file in the same directory.
+ * This source code is licensed under the MIT license found in the
+ * LICENSE file in the root directory of this source tree.
  */
 
 'use strict';
@@ -34,7 +32,7 @@ module.exports = function(babel) {
         enter: function(path) {
           // Do nothing when testing
           if (process.env.NODE_ENV === 'test') {
-            return undefined;
+            return;
           }
           // replace __DEV__ with process.env.NODE_ENV !== 'production'
           if (path.isIdentifier({name: '__DEV__'})) {
